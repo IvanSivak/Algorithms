e113a4aa4bcb37eaffe7942ba2325bb587d9e8d5
p <s>
2015-12-27 12:37:45 +0100

    Formatting clean up

--- main.js
+++ main.js
@@ -1,70 +1,108 @@
-// Prime numbers generator using Sieve of Eratosthenes
-function getPrimes(n){
-  var a = [false, false],
-      counter = 0,
-      result = [];
-      
-  for (var i = 0; i <= n - 2; i++) a.push(true);
-      
-  for (var i = 2; i <= Math.sqrt(n); i++){
-    if (a[i] == true){
-      counter = 0;
-      for (var j = i*i; j <= n; j = (i*i)+(counter*i)) {
-        a[j] = false;
-        counter++;
-      } 
-    }
-  }
-  
-  
-  /*for (var i = 0; i < a.length; i++){
-    if (a[i] == true) result.push(i);
-  }*/
-  
-  return a;//result;
-}
+/** 
+ * Prime numbers generator using Sieve of Eratosthenes
+ *
+ * More info at: 
+ * https://github.com/IvanSivak/Sieve-of-Eratosthenes
+ *
+ * Blog:
+ * 
+ *
+ */ 
+ 
+var SieveApp = (function(){
+	
+	// Entry point
+	var init = function(){
+		$(document).ready(function(){
+			setReactApp();
+		});
+	};
+	
+	/** 
+	 * Returns array of booleans where index
+	 * represents the number and its bool value
+	 * represents whether it is prime number
+	 * or not.
+	 */ 
+	var generatePrimes = function(n){
+		var a = [],
+		  counter = 0;
+		  
+	    for (var i = 0; i <= n; i++) a.push(true);
 
-//alert(getPrimes(64));
-var PrimesList = React.createClass({displayName: "PrimesList",
-    render: function() {
-      return (
-        React.createElement("div", null,
-          this.props.list.map(function(listValue, index, self){
-            return React.createElement("span", {className: 'plain' + (listValue ? ' green' : '')}, index);
-          })
-        )
-      );
-    }
-});
+	    a[0] = false;
+	    a[1] = false;
+	  
+	    for (var i = 2; i <= Math.sqrt(n); i++){
+			if (a[i] == true){
+				counter = 0;
+				for (var j = i*i; j <= n; j = (i*i)+(counter*i)) {
+					a[j] = false;
+					counter++;
+				} 
+			}
+		}
+		
+		return a;
+	};
+	
+	/** 
+	 * Renders the array
+	 */ 
+	var PrimesList = React.createClass({displayName: "PrimesList",
+		render: function() {
+		  return (
+			React.createElement("div", null,
+			  this.props.list.map(function(listValue, index, self){
+				return React.createElement("span", {className: 'plain' + (listValue ? ' green' : '')}, index);
+			  })
+			)
+		  );
+		}
+	});
 
-var SieveApp = React.createClass({
-  displayName: 'SieveApp',
+	/** 
+	 * React app wrapper
+	 * Handles the view and updates based on generated model
+	 */ 
+	var reactApp = React.createClass({
+	  displayName: 'SieveApp',
 
-  getInitialState: function getInitialState() {
-    return { list: [], limit: 139 };
-  },
-  onChange: function onChange(e) {
-    this.setState({ limit: e.target.value });
-  },
-  handleSubmit: function handleSubmit(e) {
-    e.preventDefault();
-   
-    this.setState({ list: getPrimes(this.state.limit) });
-  },
-  render: function render() {
-    return React.createElement(
-      'div',
-      null,
-      React.createElement('form', { onSubmit: this.handleSubmit },
-	    React.createElement('span', null, 'Please, enter the uppler limit'),
-        React.createElement('input', { onChange: this.onChange, value: this.state.limit, type: 'number' }),
-        React.createElement('button', null, 'Generate Primes')
-      ),
-	  React.createElement(PrimesList, { list: this.state.list })
-    );
-  }
-});
+	  getInitialState: function getInitialState() {
+		return { list: [], limit: 139 };
+	  },
+	  onChange: function onChange(e) {
+		this.setState({ limit: e.target.value });
+	  },
+	  handleSubmit: function handleSubmit(e) {
+		e.preventDefault();
+	   
+		this.setState({ list: generatePrimes(this.state.limit) });
+	  },
+	  render: function render() {
+		return React.createElement(
+		  'div',
+		  null,
+		  React.createElement('form', { onSubmit: this.handleSubmit },
+			React.createElement('span', null, 'Please, enter the uppler limit'),
+			React.createElement('input', { onChange: this.onChange, value: this.state.limit, type: 'number' }),
+			React.createElement('button', null, 'Generate Primes')
+		  ),
+		  React.createElement(PrimesList, { list: this.state.list })
+		);
+	  }
+	});
+	
+	/**
+	 * Creates and renders the react classes
+	 */
+	var setReactApp = function(){
+		ReactDOM.render(React.createElement(reactApp, null), document.getElementById('container'));
+	};
+	
+	// Exports
+	return {
+		init: init
+	}
+})();
 
-$(document).ready(function(){
-	ReactDOM.render(React.createElement(SieveApp, null), document.getElementById('container'));
-});
